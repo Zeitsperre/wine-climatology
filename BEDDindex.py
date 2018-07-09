6a00a0fc0051fa5701a493f97f0756d950502e53
Trevor James Smith <trevor_smith@live.com>
2018-07-09 14:59:18 -0400

    File cleanup

--- BEDDindex.py
+++
@@ -1,8 +0,0 @@
-##BEDD Index=name
-##maxtemperature=raster
-##mintemperature=raster
-##winklerresultscelsius=output raster
-outputs_GDALOGRRASTERCALCULATOR_1=processing.runalg('gdalogr:rastercalculator', maxtemperature,'4',mintemperature,'4',maxtemperature,'5',mintemperature,'5',maxtemperature,'6',mintemperature,'6','((((((A>=19)*19+(A<19)*A)+B)/2-10)*30)>0)*((((A>=19)*19+(A<19)*A)+B)/2-10)*30+((((((C>=19)*19+(C<19)*C)+D)/2-10)*30)>0)*((((C>=19)*19+(C<19)*C)+D)/2-10)*30+((((((E>=19)*19+(E<19)*E)+F)/2-10)*30)>0)*((((E>=19)*19+(E<19)*E)+F)/2-10)*30','-9999',5,None,None)
-outputs_GDALOGRRASTERCALCULATOR_2=processing.runalg('gdalogr:rastercalculator', maxtemperature,'7',mintemperature,'7',maxtemperature,'8',mintemperature,'8',maxtemperature,'9',mintemperature,'9','((((((A>=19)*19+(A<19)*A)+B)/2-10)*30)>0)*((((A>=19)*19+(A<19)*A)+B)/2-10)*30+((((((C>=19)*19+(C<19)*C)+D)/2-10)*30)>0)*((((C>=19)*19+(C<19)*C)+D)/2-10)*30+((((((E>=19)*19+(E<19)*E)+F)/2-10)*30)>0)*((((E>=19)*19+(E<19)*E)+F)/2-10)*30','-9999',5,None,None)
-outputs_GDALOGRRASTERCALCULATOR_3=processing.runalg('gdalogr:rastercalculator', outputs_GDALOGRRASTERCALCULATOR_1['OUTPUT'],'1',outputs_GDALOGRRASTERCALCULATOR_2['OUTPUT'],'1',maxtemperature,'10',mintemperature,'10',None,'1',None,'1','A+B+((((((C>=19)*19+(C<19)*C)+D)/2-10)*30)>0)*((((C>=19)*19+(C<19)*C)+D)/2-10)*30','-9999',5,None,None)
-outputs_SAGARASTERCALCULATOR_1=processing.runalg('saga:rastercalculator', outputs_GDALOGRRASTERCALCULATOR_3['OUTPUT'],[],'g1*(g1>0)',True,7,winklerresultscelsius)
--- CoolNightIndex.py
+++
@@ -1,4 +0,0 @@
-##Cool Night Index=name
-##minimumtemperature=raster
-##coolnightvalues=output raster
-outputs_GDALOGRRASTERCALCULATOR_1=processing.runalg('gdalogr:rastercalculator', minimumtemperature,'9',None,'1',None,'1',None,'1',None,'1',None,'1','A','-9999',5,None,coolnightvalues)
